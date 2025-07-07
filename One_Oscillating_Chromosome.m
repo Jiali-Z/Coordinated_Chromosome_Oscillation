15dea20a6858bcabdfc8714ccae5892ee294fb25
Jiali-Z <jializ@live.unc.edu>
2025-07-07 16:19:39 -0400

    Update one_oscillating_chromosome to include oscillation analysis

--- One_Oscillating_Chromosome.m
+++ One_Oscillating_Chromosome.m
@@ -3,6 +3,7 @@
 % details.
 
 
+
 clc;clear;close all;
 
 % Time Information 
@@ -14,7 +15,7 @@ n_dot = 1;
 Kct = 30; % pN/µm centromere spring constant, Harasymiw et al, 2019
 I0 = 2; % µm Rest length of centromere spring
 Kkt = 1; % pN/µm kinetocore spring constant, Cojoc et al. 2016
-Gamma = 0.5; % kg/s Drag coefficient
+Gamma = 0.25; % kg/s Drag coefficient
 Beta = 0.7; % Scaling factor
 Nmax = 25; % Maximum number of attachments
 Nbar=20; % Steady state number of MTs when Ch is centered
@@ -28,8 +29,8 @@ xL = zeros(Nsteps, 1); % Left MT tip position
 xR = zeros(Nsteps, 1); % Right MT tip position 
 NL = zeros(Nsteps, 1); % Left MT-KT attachment amount 
 NR = zeros(Nsteps, 1); % Right MT-KT attachment amount 
-cL = zeros(Nsteps, 1); % Left chromosome position 
-cR = zeros(Nsteps, 1); % Right chromosome position
+cL = zeros(Nsteps, 2,1); % Left chromosome position (time,axis,Nchromosome) 
+cR = zeros(Nsteps, 2,1); % Right chromosome position (time,axis,Nchromosome) 
 vL = zeros(Nsteps, 1); % Left chromosome position 
 vR = zeros(Nsteps, 1); % Right chromosome position
 
@@ -40,8 +41,8 @@ vR = zeros(Nsteps, 1); % Right chromosome position
 NR(1)=Nbar*(1-epsilon);
 NL(1)=Nbar*(1+epsilon); 
 % Ch position 
-cL(1) = -I0/2-epsilon;
-cR(1) = I0/2+epsilon;
+cL(1,:,1) =[-I0/2-epsilon,0];%[x,y]
+cR(1,:,1) =[ I0/2+epsilon,0];%[x,y]
 % MT tip position 
 xbar=(Kct*I0)/(Nbar*Kkt);%Steady state MT tip position 
 xL(1) = cL(1)-0.3;
@@ -63,12 +64,12 @@ for t = 1:Nsteps
 
     % Calculate velocities 
     vL(t) = (F_KT_L + F_CT_L+F_noise)/Gamma;
-    vR(t) = (F_KT_R + F_CT_R+F_noise)/Gamma;;
+    vR(t) = (F_KT_R + F_CT_R+F_noise)/Gamma;
     
     % Update Positions 
     % Calculate the current chromosome position in both directions
-    cL(t+1) = cL(t) + dt * vL(t) ;
-    cR(t+1) = cR(t) + dt * vR(t) ;
+    cL(t+1,:,1) = cL(t,:,1) + dt * vL(t) ;
+    cR(t+1,:,1) = cR(t,:,1) + dt * vR(t) ;
     % Calculate the current microtubule tip position in both directions
     xL(t+1) = xL(t) + dt * Beta * (vL(t));
     xR(t+1) = xR(t) + dt * Beta * (vR(t));
@@ -81,49 +82,56 @@ for t = 1:Nsteps
         - Lambda* NR(t));   
 end
 
-% Draw afterwards
-figure(1);
-hold on;
-plot((0:1:Nsteps-1)*dt,cR(1:Nsteps)-cL(1:Nsteps),'c','LineWidth',2);
-plot((0:1:Nsteps-1)*dt,(cL(1:Nsteps)+cR(1:Nsteps))/2,'m','LineWidth',2);
-hold off;
-xlabel({'$\tau$'}, 'Interpreter', 'latex');
-ylabel('kk distance versus Center of Mass');
-set(gca, 'FontSize', 18);
-set(gcf, 'color', 'w');
-grid on; 
-xticks(0:1:20);  % Adjust the range as needed
-
-figure(2);
-hold on;
-plot((0:Nsteps-1)*dt,NL(1:Nsteps),'c','LineWidth',2);
-plot((0:Nsteps-1)*dt,NR(1:Nsteps),'m','LineWidth',2);
-plot((0:Nsteps-1)*dt,NL(1:Nsteps)+NR(1:Nsteps),'y','LineWidth',2);
-hold off;
-xlabel({'$\tau$'}, 'Interpreter', 'latex');
-ylabel({'$\sigma{L}$'}, 'Interpreter', 'latex');
-set(gca, 'FontSize', 18);
-set(gcf, 'color', 'w');
-grid on; 
-
-figure(3);
-hold on;
-plot((0:Nsteps-1)*dt,xL(1:Nsteps),'b','LineWidth',2);
-plot((0:Nsteps-1)*dt,xR(1:Nsteps),'r','LineWidth',2);
-hold off;
-xlabel({'$\tau$'}, 'Interpreter', 'latex');
-ylabel({'$\delta{L,R}$'}, 'Interpreter', 'latex');
-set(gca, 'FontSize', 18);
-set(gcf, 'color', 'w');
-
-figure(4);
-hold on;
-plot(vR(1:Nsteps),vL(1:Nsteps),'b','LineWidth',2);
-hold off;
-xlabel({'$\tau$'}, 'Interpreter', 'latex');
-ylabel({'$\delta{L,R}$'}, 'Interpreter', 'latex');
-set(gca, 'FontSize', 18);
-set(gcf, 'color', 'w');
+% % Draw afterwards
+% figure(1);
+% hold on;
+% plot((0:1:Nsteps-1)*dt,cR(1:Nsteps)-cL(1:Nsteps),'c','LineWidth',2);
+% plot((0:1:Nsteps-1)*dt,(cL(1:Nsteps)+cR(1:Nsteps))/2,'m','LineWidth',2);
+% hold off;
+% xlabel({'$\tau$'}, 'Interpreter', 'latex');
+% ylabel('kk distance versus Center of Mass');
+% set(gca, 'FontSize', 18);
+% set(gcf, 'color', 'w');
+% grid on; 
+% xticks(0:1:20);  % Adjust the range as needed
+% 
+% figure(2);
+% hold on;
+% plot((0:Nsteps-1)*dt,NL(1:Nsteps),'c','LineWidth',2);
+% plot((0:Nsteps-1)*dt,NR(1:Nsteps),'m','LineWidth',2);
+% plot((0:Nsteps-1)*dt,NL(1:Nsteps)+NR(1:Nsteps),'y','LineWidth',2);
+% hold off;
+% xlabel({'$\tau$'}, 'Interpreter', 'latex');
+% ylabel({'$\sigma{L}$'}, 'Interpreter', 'latex');
+% set(gca, 'FontSize', 18);
+% set(gcf, 'color', 'w');
+% grid on; 
+% 
+% figure(3);
+% hold on;
+% plot((0:Nsteps-1)*dt,xL(1:Nsteps),'b','LineWidth',2);
+% plot((0:Nsteps-1)*dt,xR(1:Nsteps),'r','LineWidth',2);
+% hold off;
+% xlabel({'$\tau$'}, 'Interpreter', 'latex');
+% ylabel({'$\delta{L,R}$'}, 'Interpreter', 'latex');
+% set(gca, 'FontSize', 18);
+% set(gcf, 'color', 'w');
+% 
+% figure(4);
+% hold on;
+% plot(vR(1:Nsteps),vL(1:Nsteps),'b','LineWidth',2);
+% hold off;
+% xlabel({'$\tau$'}, 'Interpreter', 'latex');
+% ylabel({'$\delta{L,R}$'}, 'Interpreter', 'latex');
+% set(gca, 'FontSize', 18);
+% set(gcf, 'color', 'w');
 
    
 % Oscillation Analysis 
+iter = 1; % since this is one simulation run
+
+[table_raw, table_cycle, table_summary] = oscillation_measurement(cL, cR, dt, iter, true);
+
+% -------------------- Print Summary --------------------
+disp('Summary of Oscillation Amplitude and Period (Mean ± STD):');
+disp(table_summary);
